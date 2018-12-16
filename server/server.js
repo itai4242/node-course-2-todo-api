3abcccad3e25dd9354305cb053aa55b7aa771bce
itai4242 <itai4230@gmail.com>
2018-12-16 16:03:04 +0200

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -86,6 +86,7 @@ app.patch('/todos/:id', (req,res) =>{
     res.status(400).send();
   })
 })
+
 app.post('/users', (req, res) => {
   var body = _.pick(req.body,['email','password']);
   var user = new User(body);
@@ -103,6 +104,19 @@ app.post('/users', (req, res) => {
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
+
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
